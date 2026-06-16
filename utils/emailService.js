e7da1ee5657cd83e9032c56de12ffd1a40fd7e41
renderhub-found <ezenelsonifeanyichukwu@gmail.com>
2026-06-16 17:48:57 +0100

    fix: nodemailer

--- utils/emailService.js
+++ utils/emailService.js
@@ -16,16 +16,15 @@ function createTransporter() {
     return null;
   }
 
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+  const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   return transporter;
 }
