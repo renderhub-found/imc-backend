fa165805057be32d49a8a25d27c8413e28909bf7
renderhub-found <ezenelsonifeanyichukwu@gmail.com>
2026-06-23 08:35:42 +0100

    update server

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ var allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'http://localhost:3000',
-  'https://insidemycampus-five.vercel.app',
+  'https://insidemycampus.onrender.com',
   'https://insidemycampus.netlify.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
